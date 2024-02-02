2eef6356f9b947f7b5099a764e9b21cf56532d55
George Fotopoulos <xorz57@gmail.com>
2024-02-02 23:05:21 +0200

    Refactor number_generator.hpp

--- src/number_generator.hpp
+++ src/number_generator.hpp
@@ -4,25 +4,21 @@
 
 class NumberGenerator
 {
-protected:
+public:
+    NumberGenerator() : gen(rd()) {}
+
     std::random_device rd;
     std::mt19937 gen;
-
-    NumberGenerator() : gen(rd()) {}
 };
 
 template <typename T>
 class RealNumberGenerator : public NumberGenerator
 {
 private:
-    std::uniform_real_distribution<T> dis;
+    std::uniform_real_distribution<T> dis{0.0f, 1.0f};
 
 public:
-    RealNumberGenerator() : NumberGenerator(), dis(0.0f, 1.0f) {}
-
-    // random_device is not copyable
-    RealNumberGenerator(const RealNumberGenerator<T> &right)
-        : NumberGenerator(), dis(right.dis) {}
+    RealNumberGenerator() : NumberGenerator() {}
 
     float get() { return dis(gen); }
 
@@ -36,29 +32,17 @@ public:
 template <typename T>
 class RNG
 {
-private:
-    static RealNumberGenerator<T> gen;
-
 public:
     static T get() { return gen.get(); }
 
     static float getUnder(T max) { return gen.getUnder(max); }
 
-    static uint64_t getUintUnder(uint64_t max)
-    {
-        return static_cast<uint64_t>(gen.getUnder(static_cast<float>(max) + 1.0f));
-    }
-
     static float getRange(T min, T max) { return gen.getRange(min, max); }
 
     static float getRange(T width) { return gen.getRange(width); }
 
-    static float getFullRange(T width)
-    {
-        return gen.getRange(static_cast<T>(2.0f) * width);
-    }
-
-    static bool proba(float threshold) { return get() < threshold; }
+private:
+    static RealNumberGenerator<T> gen;
 };
 
 using RNGf = RNG<float>;
@@ -71,10 +55,6 @@ class IntegerNumberGenerator : public NumberGenerator
 {
 public:
     IntegerNumberGenerator() : NumberGenerator() {}
-
-    // random_device is not copyable
-    IntegerNumberGenerator(const IntegerNumberGenerator<T> &right)
-        : NumberGenerator() {}
 
     T getUnder(T max)
     {
@@ -92,19 +72,13 @@ public:
 template <typename T>
 class RNGi
 {
-private:
-    static IntegerNumberGenerator<T> gen;
-
 public:
     static T getUnder(T max) { return gen.getUnder(max); }
-
     static T getRange(T min, T max) { return gen.getRange(min, max); }
+
+private:
+    static IntegerNumberGenerator<T> gen;
 };
 
 template <typename T>
 IntegerNumberGenerator<T> RNGi<T>::gen;
-
-using RNGi32 = RNGi<int32_t>;
-using RNGi64 = RNGi<int64_t>;
-using RNGu32 = RNGi<uint32_t>;
-using RNGu64 = RNGi<uint64_t>;
