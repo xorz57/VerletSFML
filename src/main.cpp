d1836e1992ecc65bb14a2532783c27d1aebe88e2
George Fotopoulos <xorz57@gmail.com>
2024-02-02 23:15:50 +0200

    Refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,13 @@ static sf::Color getRainbow(float t)
     const float r = sin(t);
     const float g = sin(t + 0.33f * 2.0f * Math::PI);
     const float b = sin(t + 0.66f * 2.0f * Math::PI);
-    return {static_cast<uint8_t>(255.0f * r * r),
-            static_cast<uint8_t>(255.0f * g * g),
-            static_cast<uint8_t>(255.0f * b * b)};
+    return {
+        static_cast<uint8_t>(255.0f * r * r),
+        static_cast<uint8_t>(255.0f * g * g),
+        static_cast<uint8_t>(255.0f * b * b)};
 }
 
-int32_t main(int32_t, char *[])
+int main()
 {
     // Create window
     constexpr int32_t window_width = 1000;
@@ -23,8 +24,7 @@ int32_t main(int32_t, char *[])
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 1;
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Verlet",
-                            sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "VerletSFML", sf::Style::Default, settings);
     const uint32_t frame_rate = 60;
     window.setFramerateLimit(frame_rate);
 
@@ -32,9 +32,7 @@ int32_t main(int32_t, char *[])
     Renderer renderer{window};
 
     // Solver configuration
-    solver.setConstraint({static_cast<float>(window_width) * 0.5f,
-                          static_cast<float>(window_height) * 0.5f},
-                         450.0f);
+    solver.setConstraint({static_cast<float>(window_width) * 0.5f, static_cast<float>(window_height) * 0.5f}, 450.0f);
     solver.setSubStepsCount(8);
     solver.setSimulationUpdateRate(frame_rate);
 
@@ -54,24 +52,19 @@ int32_t main(int32_t, char *[])
         sf::Event event{};
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed ||
-                sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
             {
                 window.close();
             }
         }
 
-        if (solver.getObjectsCount() < max_objects_count &&
-            clock.getElapsedTime().asSeconds() >= object_spawn_delay)
+        if (solver.getObjectsCount() < max_objects_count && clock.getElapsedTime().asSeconds() >= object_spawn_delay)
         {
             clock.restart();
-            auto &object = solver.addObject(
-                object_spawn_position,
-                RNGf::getRange(object_min_radius, object_max_radius));
+            auto &object = solver.addObject(object_spawn_position, RNGf::getRange(object_min_radius, object_max_radius));
             const float t = solver.getTime();
             const float angle = max_angle * sin(t) + Math::PI * 0.5f;
-            solver.setObjectVelocity(
-                object, object_spawn_speed * sf::Vector2f{cos(angle), sin(angle)});
+            solver.setObjectVelocity(object, object_spawn_speed * sf::Vector2f{cos(angle), sin(angle)});
             object.color = getRainbow(t);
         }
 
